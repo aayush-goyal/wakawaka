c69055e19ea238e7a6a29e8740abe5772f8d5e6a
Aayush Goyal <aayushgoyal.mps@gmail.com>
2024-10-04 02:04:01 +0530

    Add enums for different unit and chart types.
    Signed-off-by: Aayush Goyal <aayushgoyal.mps@gmail.com>

---
+++ src/data/model/chart-type.ts
@@ -0,0 +1,36 @@
+/* eslint-disable no-unused-vars */
+// Import global from third party libraries.
+
+// Import use cases.
+
+// Import controllers.
+
+// Import middlewares.
+
+// Import models.
+// Entities.
+// Enums.
+// Errors.
+
+// Import utilities.
+
+// Import DB.
+
+/**
+ * This enum defines the different chart types the user can have data in.
+ *
+ * @version 1.0.0
+ * @author Aayush Goyal
+ * @created 2024-10-04
+ * @modifier
+ * @modified
+ */
+enum CHART_TYPE {
+    BAR_CHART_HORIZONTAL,
+    BAR_CHART_VERTICAL,
+    DOUGHNUT_CHART,
+    NIGHTINGALE_CHART,
+    PIE_CHART
+}
+
+export default CHART_TYPE;
---
+++ src/data/model/data-type.ts
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+// Import global from third party libraries.
+
+// Import use cases.
+
+// Import controllers.
+
+// Import middlewares.
+
+// Import models.
+// Entities.
+// Enums.
+// Errors.
+
+// Import utilities.
+
+// Import DB.
+
+/**
+ * This enum defines the unit of stats the user can have data in
+ *
+ * @version 1.0.0
+ * @author Aayush Goyal
+ * @created 2024-10-04
+ * @modifier
+ * @modified
+ */
+enum DATA_TYPE {
+    PERCENT,
+    TIME
+}
+
+export default DATA_TYPE;
